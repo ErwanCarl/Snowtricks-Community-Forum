ecf142ebe4868b31a79474ac97ec0c959d2e2d59
ErwanCarl <erwan.carlini@orange.fr>
2023-02-27 20:30:14 +0100

    logo-settings

--- src/Controller/AccountSettingController.php
+++ src/Controller/AccountSettingController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\LogoFormType;
 use App\Form\LostPasswordType;
+use Doctrine\ORM\EntityManager;
 use App\Service\FormInputHandler;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,6 +28,8 @@ class AccountSettingController extends AbstractController
         $lostPasswordForm = $this->createForm(LostPasswordType::class, $user);
         $lostPasswordForm->handleRequest($request);
 
+        $logoForm = $this->createForm(LogoFormType::class);
+        
         if ($lostPasswordForm->isSubmitted() && $lostPasswordForm->isValid()) {
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
@@ -46,7 +49,37 @@ class AccountSettingController extends AbstractController
 
         return $this->render('accountsettings/account-settings.html.twig', [
             'user' => $user,
-            'lostPasswordForm' => $lostPasswordForm->createView()
+            'lostPasswordForm' => $lostPasswordForm->createView(),
+            'logoForm' => $logoForm->createView()
         ]);
+    }
+
+    #[Route('/logosettings', name: 'app_logo_settings')]
+    public function logoSettings(Request $request, LogoFormType $logoForm, UserRepository $userRepository): Response
+    {
+        $logoForm = $this->createForm(LogoFormType::class);
+        $logoForm->handleRequest($request);
+
+        if ($logoForm->isSubmitted() && $logoForm->isValid()) {
+            /** @var \App\Entity\User $user */
+            $user = $this->getUser();
+            $user->setLogo($logoForm->get('logoChoice')->getData());
+
+            $userRepository->save($user, true);
+
+            $this->addFlash(
+                'success',
+                'Votre choix de logo a bien été enregistré.'
+            );
+
+            return $this->redirectToRoute('app_account_settings');
+        }
+
+        $this->addFlash(
+            'danger',
+            'L\'enregitrement de votre choix a échoué, veuillez retenter.'
+        );
+
+        return $this->redirectToRoute('app_account_settings');
     }
 }
